767f134e5158acf6c1003afcc91814125d17258f
Amit Nivedan Kalra <AMITNKALRA@amits-air.attlocal.net>
2015-08-09 12:31:10 -0700

    Initial Commit

---
+++ Dice!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dice!
+//
+//  Created by Amit Nivedan Kalra on 8/9/15.
+//  Copyright (c) 2015 Amit Nivedan Kalra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
